e6da526417eaf8dd4b733131093c400d35bf27d4
ramarfx <ramagamiing33@gmail.com>
2023-11-28 22:58:45 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container:{
+      center: true,
+      padding:'1rem',
+    },
+    extend: {
+      colors:{
+        primary: '#0ea5e9',
+        secondary: '#94a3b8',
+        dark: '#0f172a',
+        lightDark:{
+          100: '#283e71',
+          200: '#1c2c50',
+        }
+
+      },
+      screens:{
+        '2xl': '1320'
+      }
+    },
+    fontFamily:{
+      poppins: ['poppins', ...defaultTheme.fontFamily.sans]
+    },
+  },
+  plugins: [
+    
+
+  ],
+}
+
